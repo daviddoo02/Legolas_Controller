0f6fea5fc1f4bc9d8df06920a5bd62236773d694
Ubuntu <homandat2002@gmail.com>
2023-12-19 16:32:21 -0600

    Servo Publisher online

--- inverse_kinematics/src/activate_servo.py
+++ inverse_kinematics/src/activate_servo.py
@@ -55,35 +55,12 @@ class Biped:
         foreleg_angle = self.knee_callback(msg)
         calf_angle = self.calf_callback(msg)
 
-        # sf1, sf2, sf3 = 0.1, 0.1, 0.1
-
-        # thigh_smoothed = (thigh_angle * sf1) + (self.thigh_prev * (1-sf1))
-        # foreleg_smoothed = (foreleg_angle * sf2) + (self.foreleg_prev * (1-sf2))
-        # calf_smoothed = (calf_angle * sf3) + (self.calf_prev * (1-sf3))
-
-        # self.thigh_prev = thigh_smoothed
-        # self.foreleg_prev = foreleg_smoothed
-        # self.calf_prev = calf_smoothed
-
-        # self.thigh_servo.angle = thigh_smoothed
-        # self.foreleg_servo.angle = foreleg_smoothed
-        # self.calf_servo.angle = calf_smoothed
-
         self.thigh_servo.angle = thigh_angle
         self.foreleg_servo.angle = foreleg_angle
         self.calf_servo.angle = calf_angle
 
         
-
 if __name__ == '__main__':
     rospy.init_node('biped')
 
     biped = Biped()
-
-    # Declare this after ros spin
-
-    # i2c = busio.I2C(SCL, SDA)
-
-    # pca = PCA9685(i2c)
-
-    # pca.deinit()
--- ros_servo/src/Servo_Publisher.py
+++ ros_servo/src/Servo_Publisher.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+  
+import rospy
+import typing
+from ros_servo.msg import servo_reading
+
 import time
 
 from board import SCL, SDA
@@ -11,57 +17,82 @@ from adafruit_ads1x15.analog_in import AnalogIn
 
 import matplotlib.pyplot as plt
 
-
-
-i2c = busio.I2C(SCL, SDA)
-
-ads = ADS.ADS1115(i2c)
-
-chan = AnalogIn(ads, ADS.P0)
-
-pca = PCA9685(i2c)
-
-pca.frequency = 50
-
-
-servo = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2600, actuation_range=270)
-
-# servo.angle = 0
-
-
-def map_range(x, in_min, in_max, out_min, out_max):
-    """re-maps a number from one range to another."""
-    mapped = (x-in_min) * (out_max - out_min) / (in_max-in_min) + out_min
-    if out_min <= out_max:
-        return max(min(mapped, out_max), out_min)
-    return min(max(mapped, out_max), out_min)
-
-def pot_2_deg(Vout, Vmax = 7, servo_range = 270):
-    angle = 687* Vout/Vmax - 25.64
-    return angle
-
-readings = []
-
-try:
-    while True:
-        desiredPos=float(input("Degrees (0-270): "))
-        servo.angle = desiredPos
-
-        # curr_read = map_range(chan.value, 2048, 24150, 0, 270)
-
-        curr_read = pot_2_deg(chan.voltage)
-
-        while abs(desiredPos - curr_read) > 1:
-            readings.append(curr_read)
-            curr_read = pot_2_deg(chan.voltage)
-            print(curr_read)
-
+class Servo:
+    def __init__(self) -> None:
         
-        Position = curr_read
-        print("     Channel value:", chan.value)
-        print("     Current Position:", Position)
-except:
-    plt.plot(readings)
-    plt.ylabel('response')
-    plt.show()
+        i2c = busio.I2C(SCL, SDA)
 
+        ads = ADS.ADS1115(i2c)
+
+        self.chan0 = AnalogIn(ads, ADS.P0)
+
+        self.pca = PCA9685(i2c)
+
+        self.pca.frequency = 50
+
+        self.servo = servo.Servo(self.pca.channels[0], min_pulse=500, max_pulse=2600, actuation_range=270)
+
+        self.servo.angle = 0
+
+        pub_topic = 'servo_reading'
+
+        self.servo_pub = rospy.Publisher(pub_topic, servo_reading, queue_size=1)
+        
+        rospy.init_node('ros_servo')
+
+        rate = rospy.Rate(1)
+
+        self.servo_msg = servo_reading()
+
+        while not rospy.is_shutdown():
+            self.constant_value_test()
+            rate.sleep()
+
+    def map_range(self, x: float, in_min: float, in_max: float, out_min: float, out_max: float) -> float:
+        """
+        Map a number from one range to another linearly and clamp the result.
+
+        Parameters:
+        - x (float): Input value to be mapped.
+        - in_min (float): Minimum value of the input range.
+        - in_max (float): Maximum value of the input range.
+        - out_min (float): Minimum value of the output range.
+        - out_max (float): Maximum value of the output range.
+
+        Returns:
+        - float: Mapped value within the specified output range.
+        """
+        # Calculate the mapped value, ensuring it's within the output range
+        mapped = max(min((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min, out_max), out_min)
+        return mapped
+
+    def enc_2_deg(self, V_reading, Vmin = 0.262, Vmax = 3.053):
+        angle = self.map_range(V_reading, Vmin, Vmax, 0, 270)
+        return angle
+
+    def constant_value_test(self):
+        desiredPos = float(input("Degrees (0-270): "))
+        self.servo.angle = desiredPos
+
+        print("{:>5}\t{:>5}\t{:>5}".format('raw', 'v', 'deg'))
+        n = 0
+
+        while n < 20:
+            value = self.chan0.value
+            voltage = self.chan0.voltage
+            angle = self.enc_2_deg(self.chan0.voltage)
+
+            self.servo_msg.encoder_value = value
+            self.servo_msg.encoder_voltage = voltage
+            self.servo_msg.servo_angle = angle
+            self.servo_pub.publish(self.servo_msg)
+
+
+            print("{:>5}\t{:>5.3f}\t{:>5.3f}".format(value, voltage, angle))
+            n += 1
+
+if __name__ == '__main__':
+    try:
+        ros_servo = Servo()
+    except rospy.ROSInterruptException:
+        pass
